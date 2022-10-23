608d7c552119a67a2a605fa61200a06dd55b0c90
ub <kimub1204@naver.com>
2022-10-23 17:51:29 +0900

    init: styled-components, @types/styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-
+import { MyComponent } from "./style/globalStyle";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -27,6 +27,7 @@ function App() {
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
+      <MyComponent>StyleComponents</MyComponent>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./style/globalStyle";
+import theme from "./style/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider {...{ theme }}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
