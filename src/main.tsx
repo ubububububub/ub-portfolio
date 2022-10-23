dab8cf7f293c5fbb8ab9cadfcf7565b7969ef1ca
ub <kimub1204@naver.com>
2022-10-23 17:51:29 +0900

    init: styled-components, @types/styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./style/globalStyle";
+import theme from "./style/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider {...{ theme }}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
